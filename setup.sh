c118ef8b50f35633b06e92c0ae7496512dd8b678
Arjun Sivakumar <arjun.sivakumar@trilogy.com>
2025-11-28 07:58:43 +0000

    env fix

--- setup.sh
+++ setup.sh
@@ -56,7 +56,7 @@ cat << EOF
   "env": {
     "OAUTH_CLIENT_SECRET": "./credentials/client-secrets.json",
     "OAUTH_CLIENT_TOKEN": "./.gcp-saved-token.json"
-    "BRAINLIFT_API_URL": "https://okmztotkjfpmqstgkqsh.supabase.co/rest/v1"
+    "BRAINLIFT_API_URL": "https://brainlift.space/functions/api/v1"
   },
   "cwd": "$(pwd)"
 }
